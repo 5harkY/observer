0e2f2ea912346d52097a5a22a480b09e60a52d4b
Ivan Platonov <iplatonov@phoenixit.ru>
2023-02-18 23:13:45 +0200

    Remove in_app report calculation

--- app/services/report.rb
+++ app/services/report.rb
@@ -3,12 +3,6 @@
 module Services
   class Report
     def call(address:, start_time:, end_time:)
-      calc_in_db(address:, start_time:, end_time:)
-    end
-
-    private
-
-    def calc_in_db(address:, start_time:, end_time:)
       addr = IpAddress.find_by(address:)
       return {} unless addr
 
@@ -25,65 +19,6 @@ module Services
       lost_percentage = base_query.select('AVG( (NOT success)::int * 100) AS val').first['val'].to_f
 
       result.merge(lost_percentage:)
-    end
-
-    def calc_in_app(address:, start_time:, end_time:)
-      addr = IpAddress.find_by(address:)
-      return {} unless addr
-
-      observations = addr.observation_results.where('created_at >= ?', start_time)
-        .where('created_at <= ?', end_time).order(:rtt)
-      return {} unless observations
-
-      success_observations = observations.where(success: true)
-      success_rtts = success_observations.pluck(:rtt)
-      avg_rtt = avg(success_rtts)
-
-      {
-        avg_rtt:,
-        min_rtt: success_rtts.first || 0,
-        max_rtt: success_rtts.last || 0,
-        median_rtt: median(success_rtts),
-        std_deviation: std_deviation(success_rtts, avg_rtt),
-        lost_percentage: lost_percentage(success_observations.count, observations.count),
-        observations: observations.count,
-        success_observations: success_observations.count
-      }
-    end
-
-    def avg(arr)
-      return 0 if arr.count.zero?
-
-      arr.sum / arr.count
-    end
-
-    def median(arr)
-      len = arr.length
-      return 0 if len.zero?
-      return arr[0] if len == 1
-
-      (arr[(len - 1) / 2] + arr[len / 2]) / 2.0
-    end
-
-    def variance(arr, mean)
-      return 0 if arr.empty?
-
-      sum = 0.0
-      arr.each { |v| sum += (v - mean)**2 }
-      sum / (arr.size - 1)
-    end
-
-    def std_deviation(arr, mean)
-      return 0 if arr.empty?
-
-      Math.sqrt(variance(arr, mean))
-    end
-
-    def lost_percentage(success, total)
-      return 0 if total.zero?
-
-      failed = total - success
-      failed * 100.0 / total
     end
   end
 end
